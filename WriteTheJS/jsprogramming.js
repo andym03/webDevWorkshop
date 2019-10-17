26628edc1e79375fc9887bbcd9d29368b077925f
Andrew McMenemy <andrew.mcmenemy03@gmail.com>
2019-10-17 15:32:59 +1100

    Added more starter code, helper functions

--- WriteTheJS/jsprogramming.js
+++ WriteTheJS/jsprogramming.js
@@ -4,14 +4,21 @@ var plusMinusTest2 = [1, 2, 3, -1, -2, -3, 0, 0];
 // Counter
 // Retrieve the current text in the answer field. If it is a number, increase it by 1. Else set it to 0.
 // Hint: Use the function isNaN (is Not a Number)
-// Hint: Use the existing function written for you called getCurrentAnswer();
 function increment() {
+    var curr = getCurrentAnswer();
+    if (isNaN(curr)) {
+        // Not a number logic
+    } else {
+        // Increment
+    }
+
+    setAnswer(curr);
 }
 
 // Change Colour
 /* Pick a random colour from the list and change the background
-   Hint: You need to use two functions from the Math object.
-         Math.floor(), and Math.random()
+   Hint: 
+   colours[Math.floor(Math.random()*colours.length)] picks a random item from the list
    Set the background with document.body.style.backgroundColor
 */
 // Bonus: When colour is black, make all text white. When No longer black, make all text black */ 
@@ -27,9 +34,21 @@ function changeColor() {
         answer[0] is the percentage of positive values
         answer[1] is the percentage of negative values 
         answer[2] is the percentage of zero values
+
+    For example, data will be an array that looks like: [-4, 3, -9, 0, 4, 1]
+    It has 3 positive values, 2 negative values and a single 0. It also has a length of 6. 
+
+    data.length will return the length of the array.
+
+
 */
 function plusMinus(data) {
+    var answer = [];
+    for (var i = 0; i < data.length; i++) {
+        
+    }
 
+    return answer
 }
 
 // GetTime
@@ -68,29 +87,35 @@ function getCurrentAnswer() {
     return document.getElementsByTagName("h4")[0].innerHTML;
 }
 
-function plusMinus1(plusMinusTest1) {
-    checkPlusMinus1(plusMinus(plusMinusTest1));
+function setAnswer(data) {
+    document.getElementsByTagName("h4")[0].innerHTML = data;
 }
 
-function plusMinus2(plusMinusTest2) {
-    checkPlusMinus1(plusMinus(plusMinusTest2));
+/*
+    The function called by the first plus minus button. Do not modify
+*/
+function plusMinus1() {
+    checkPlusMinus1(plusMinus(plusMinusTest1), plusMinusAns1);
 }
 
-function checkPlusMinus1(answer) {
-    if (answer == undefined) {
+/*
+    The function called by the second plus minus button. Do not modify
+*/
+function plusMinus2() {
+    checkPlusMinus1(plusMinus(plusMinusTest2), plusMinusAns2);
+}
+
+/*
+    The checker function for both plus minus functions. Takes in the user result and the given answer
+    set for either plusMinus1 or plusMinus2. Answers are at the bottom of the page, input at the top.
+    Do not modify.
+*/
+function checkPlusMinus1(result, answer) {
+    if (result == undefined) {
         document.getElementsByTagName("h4")[0].innerHTML = "Wrong!";
     }
-    for (var i = 0; i < answer.length; i++) {
-        if (answer[i] != plusMinusAns1[i]) {
-            document.getElementsByTagName("h4")[0].innerHTML = "Wrong!";
-        }
-    }
-    document.getElementsByTagName("h4")[0].innerHTML = "Correct!";
-}
-
-function checkPlusMinus2(answer) {
-    for (var i = 0; i < answer.length; i++) {
-        if (answer[i] != plusMinusAns2[i]) {
+    for (var i = 0; i < result.length; i++) {
+        if (result[i] != answer[i]) {
             document.getElementsByTagName("h4")[0].innerHTML = "Wrong!";
         }
     }
